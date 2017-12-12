ca239e3d973ff0263f9a71ae7c5351122d191321
jinweizhao <694951521@qq.com>
2017-12-12 18:20:56 +0800

    begin interactiveTransition

--- Demo/InteractiveTransition.swift
+++ Demo/InteractiveTransition.swift
@@ -8,6 +8,29 @@
 
 import UIKit
 
-class InteractiveTransition: NSObject {
+
+enum InteractiveDirection: Int {//手势的方向
+    case Left
+    case Right
+    case Up
+    case Down
+}
+
+enum InteractiveType: Int {//手势控制哪种转场
+    case Present
+    case Dismiss
+    case Push
+    case Pop
+}
+
+
+class InteractiveTransition: UIPercentDrivenInteractiveTransition {
+    /**记录是否开始手势，判断pop操作是手势触发还是返回键触发*/
+    var interaction = false
+    
+    
+    
+    
+    
     
 }
