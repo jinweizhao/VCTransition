e7d69442db3c4b3fcf8f559705d30c20e0c38b66
jinweizhao <694951521@qq.com>
2017-12-14 22:36:10 +0800

    complete interactivetransition

--- Demo/InteractiveTransition.swift
+++ Demo/InteractiveTransition.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+typealias GestureConfig = () -> ()
 
 enum InteractiveDirection: Int {//手势的方向
     case Left
@@ -28,9 +29,112 @@ class InteractiveTransition: UIPercentDrivenInteractiveTransition {
     /**记录是否开始手势，判断pop操作是手势触发还是返回键触发*/
     var interaction = false
     
+    var presentConfig : GestureConfig?
+    
+    var pushConfig: GestureConfig?
+    
+    fileprivate var vc: UIViewController?
+    
+    fileprivate var interactiveDirection: InteractiveDirection?
+    
+    fileprivate var interactiveType: InteractiveType?
+    
+//    class func interactiveTransitionWithTransitionType(type: InteractiveType,gestureDirection direction: InteractiveDirection) {
+//
+//    }
+    
+    init(WithTransitionType type: InteractiveType,gestureDirection direction: InteractiveDirection){
+        self.interactiveDirection = direction
+        self.interactiveType = type
+    }
     
     
     
+    
+    func addGestureForViewController(vc: UIViewController) {
+        let pan = UIPanGestureRecognizer.init(target: self, action: #selector(handleGesture(gesture:)))
+        
+        self.vc = vc
+        
+        vc.view.addGestureRecognizer(pan)
+        
+    }
+    @objc
+    func handleGesture(gesture: UIPanGestureRecognizer) {
+        
+        var percent: CGFloat = 0.0
+        
+        switch interactiveDirection! {
+        case .Left:
+            let transitionX = -gesture.translation(in: gesture.view).x
+            percent = transitionX
+        case .Right:
+            let transitionX = gesture.translation(in: gesture.view).x
+            percent = transitionX
+        case .Up:
+            let transitionX = -gesture.translation(in: gesture.view).y
+            percent = transitionX
+        case .Down:
+            let transitionX = gesture.translation(in: gesture.view).y
+            percent = transitionX
+        }
+        
+        switch gesture.state {
+        case .began:
+            //手势开始的时候标记手势状态，并开始相应的事件
+            self.interaction = true
+            self.startGesture()
+        case .changed:
+            //手势过程中，通过updateInteractiveTransition设置pop过程进行的百分比
+            self.update(percent)
+        case .ended:
+            //手势完成后结束标记并且判断移动距离是否过半，过则finishInteractiveTransition完成转场操作，否者取消转场操作
+            self.interaction = false
+            if percent > 0.5 {
+                self.finish()
+            }else {
+                self.cancel()
+            }
+        default:()
+            
+        }
+    }
+    
+    func startGesture() {
+        
+        switch interactiveType! {
+        case .Present:
+            if self.presentConfig != nil {
+                self.presentConfig!()
+            }
+        case .Dismiss:
+            vc?.dismiss(animated: true, completion: nil)
+        case .Push:
+            if self.pushConfig != nil {
+                self.pushConfig!()
+            }
+        case .Pop:
+            vc?.navigationController?.popViewController(animated: true)
+        }
+        
+    }
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
